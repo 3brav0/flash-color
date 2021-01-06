8a6e9434535c176034e3337cfe276ff9782ffd36
Elias Bravo <spectrevolve@outlook.com>
2021-01-06 00:19:57 -0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,37 @@
 import logo from './logo.svg';
 import './App.css';
 
+import React from 'react';
+import {HuePicker} from 'react-color';
+
+class Color extends React.Component {
+  state = {
+    background: '#fff',
+  };
+
+  handleChange = (color) => {
+    this.setState({ background: color.hex });
+  };
+
+  render() {
+    return (
+      <div style={{width: '100vw', height: '100vh', backgroundColor: this.state.background, display: 'flex', justifyContent: 'center'}}>
+        <div style={{position: 'fixed', bottom: 0}}>
+          <HuePicker
+            color={ this.state.background }
+            onChange={ this.handleChange }
+          />
+        </div>
+      </div>
+      
+    );
+  }
+}
+
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -17,7 +44,8 @@ function App() {
         >
           Learn React
         </a>
-      </header>
+      </header> */}
+      <Color/>
     </div>
   );
 }
